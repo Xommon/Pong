2b3c0cfe7b532b36765ffb6eac47c61508e94108
Micheal Quentin <mquenti3@gmail.com>
2019-03-19 12:48:52 -0500

    Right and left paddle distinction

--- Pong/Assets/Paddle.cs
+++ Pong/Assets/Paddle.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+	//Variables for paddle
+	float speed;
+	float height;
+
+	string input;
+	bool isRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        height = transform.localScale.y;
+        speed = 5f;
     }
 
     public void Init(bool isRightPaddle) {
+
+    	isRight = isRightPaddle;
 
     	Vector2 pos = Vector2.zero;
 
     	if (isRightPaddle) {
     		pos = new Vector2 (GameManager.topRight.x,0);
     		pos -= Vector2.right * transform.localScale.x; //Move a bit to the left
-    	}
-    	else {
+    	
+    		input = "PaddleRight";
+    	} else {
     		pos = new Vector2 (GameManager.bottomLeft.x,0);
     		pos += Vector2.right * transform.localScale.x; //Move a bit to the right
+    	
+    		input = "PaddleLeft";
     	}
 
     	//Update the paddle's position
